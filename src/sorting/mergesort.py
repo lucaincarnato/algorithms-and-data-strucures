27b3eef0b7791038bcaef14e8825eea12f7994d3
lucaincarnato <lucaincarnato.03@gmail.com>
2022-04-09 16:24:28 +0200

    Deleted wrong script and set it in default

--- src/sorting/mergesort.py
+++ src/sorting/mergesort.py
@@ -1,17 +1,9 @@
-from heapq import merge
-
+'''
+Script created by Luca Maria Incarnato
+GitHub: lucaincarnato
+Instagram: luca_incarnato
+Twitter: incarnato_luca
+'''
 
 def mergesort(S:list):
-    if len(S) == 2:
-        S[0], S[1] = min(S), max(S)
-    else:
-        index = len(S)//2
-        S1 = S[:index]
-        S2 = S[index:]
-        L1 = mergesort(S1)
-        L2 = mergesort(S2)
-        L = L1 + L2
-        return L
-#! TypeError: unsupported operand type(s) for +: 'NoneType' and 'NoneType'
-
-mergesort([3, 1, 4, 5])
+    pass
